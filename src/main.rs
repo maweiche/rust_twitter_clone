a57c574da29737b09ea17a47b612a4256dacdb37
Matt <maweiche@gmail.com>
2024-01-04 18:37:34 +0100

    added routes into main.rs

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate actix_web;
+
+use std::{env, io};
+
+use actix_web::{middleware, App, HttpServer};
+
+mod constants;
+mod like;
+mod response;
+mod tweet;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server-info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+        .wrap(middleware::Logger::default())
+        .service(tweet::list)
+        .service(tweet::get)
+        .service(tweet::create)
+        .service(tweet::delete)
+        .service(like::list)
+        .service(like::plus_one)
+        .service(like::minus_one)
+    })
+    .bind("0.0.0.0:9090")?
+    .run()
+    .await
+}
